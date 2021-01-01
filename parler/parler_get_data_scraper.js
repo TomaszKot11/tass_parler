60cb4bed4d76f2e570b0b0c81709c1f35c069a89
TomaszKot11 <kottomasz98@gmail.com>
2021-01-01 23:33:25 +0100

    update

--- parler/parler_get_data_scraper.js
+++ parler/parler_get_data_scraper.js
@@ -1,6 +1,9 @@
 const { exec } = require('child_process');
 const fs = require('fs');
 
+const MST = 's%3ALcDqosWVeX24zx2Vyx7UDbnn8TIBUitHGkzMrCSpDaSjlQ9RlBRUlot7Y9tofA7AXy7oaP9Ta5Ps34r7A3qDs54pViVnkdc8SKhMqW8UNPAmU20Z67X7BBgf9ti7EGTC.sEC6TLc8kP%2FAaKNLGPJzai8kRjxoWfrusit9bxX0Ksc';
+const JST = 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzZXNzaW9uSWQiOjE4MjkwNzIyLCJ1c2VySWQiOjEyMzE1MjI1LCJwZXJtaXNzaW9ucyI6MjU2LCJ2ZXJpZmllZCI6ZmFsc2UsImlhdCI6MTYwODMyNjU0NywiZXhwIjoxNjA4MzI2ODQ3fQ.QHTyK4HiNyEGDNeWb8arv4-Qst7v92JYFvEceUamPeXJcVdQySHZR3ZIyM8viFHpfZSXHOpASxKpCTRxOapEwQ';
+
 // https://github.com/castlelemongrab/parlance
 const sampleAccounts = ['Johnnyjoe1083', 'LHolden', 'BuddyDavis21', 'Compatriot'];
 
@@ -17,7 +20,7 @@ const serializeToFile = (object, fileName) => {
 }
 
 fetchData = (accountName, packageOption, fileName) => {
-    exec(`parlance ${packageOption} -u ${accountName} -c config.json`, (err, stdout, stderr) => {
+    exec(`parlance ${packageOption} -u ${accountName} -c auth.json`, (err, stdout, stderr) => {
         if (err) {
             console.log(`Error while fetching for ${accountName} ${err}`);
             return;
